9eb7e47b4fd312bd46cacb85f82afe6810d2b237
adambabusek <adam.babusek@gmail.com>
2017-10-17 22:49:55 +0200

    loading of organization data from http get result

--- src/app/searchForm.ts
+++ src/app/searchForm.ts
@@ -17,14 +17,21 @@ export class SearchForm  {
   constructor(private searchService: SearchService) { }
 
   submit(): void {
+    this.company = null;
     this.searchService.findInspections(this.searchIco)
                       .then((resp: any) => {                            
-                            this.company = new Organization();
-                            this.company.ico = this.searchIco;
-                            this.company.name="Ajajaj s.r.o.";
-                            this.company.place="č.p. 12/A, 100 00 Praha";
+                            if (!resp) {
+                              return;
+                            }
 
-                            if (resp && resp.inspections) {                              
+                            if (resp.organization) {
+                              this.company = new Organization();
+                              this.company.ico = resp.organization.ico;
+                              this.company.name = resp.organization.name;
+                              this.company.place = resp.organization.place;
+                            }
+
+                            if (resp.inspections) {                              
                               this.resultCnt = resp.inspections.length;
                             }
                             this.notify.emit(resp);
